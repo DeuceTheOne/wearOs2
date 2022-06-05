f0093f2f3745022bd8d9585193a4df8bd93199ad
DeuceTheOne <106890290+DeuceTheOne@users.noreply.github.com>
2022-06-04 22:03:48 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/wearos2/MainActivity.kt
+++ app/src/main/java/com/example/wearos2/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.wearos2
 import android.app.Activity
 import android.os.Bundle
 import com.example.wearos2.databinding.ActivityMainBinding
+//enlaza elementos con el codigo    
 
 class MainActivity : Activity() {
-
+    
+    //asigna un valor
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //modifica el valor del textview
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
